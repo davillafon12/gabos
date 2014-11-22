15b8c42bff2dd6059fb24d823f6282b5be6e1582
davillafon12 <davillafon12@gmail.com>
2014-11-22 15:13:16 -0600

    Cambio Menu - Recibos
    Se cambio el menu principal para poder acceder las opciones de recibos
    por dinero

--- application/views/Header/Menu_Principal.php
+++ application/views/Header/Menu_Principal.php
@@ -55,14 +55,21 @@
 			<img class='icono' src=".$Ruta_Icono_Contabilidad." alt='Contabilidad' width='22' height='22'>
 			<a href='#' class='dir'>Contabilidad</a>	
 			<ul>
-				<li class='first_Level_2'><a href=".$Ruta_Base."contabilidad/recibos>Recibos de dinero</a></li>
+				<li class='first_Level_2'><a href='#'>Cambio de código</a></li>
+				<li><a href='#' class='dir'>Recibos de Dinero</a>
+					<ul>
+						<li class='first'><a href=".$Ruta_Base."contabilidad/recibos>Crear</a></li>
+						<li><a href=".$Ruta_Base."contabilidad/recibos/anular>Anular</a></li>
+						<li class='last'><a href=".$Ruta_Base."contabilidad/recibos/deposito>Confirmar Deposito</a></li>
+					</ul>
+				</li>
 				<li><a href='#' class='dir'>Notas</a>
 					<ul>
 						<li class='first'><a href='#'>Débito</a></li>
 						<li class='last'><a href=".$Ruta_Base."contabilidad/notas/notasCredito>Crédito</a></li>
 					</ul>
 				</li>				
-				<li><a href='#'>Cambio de código</a></li>
+				
 				<li class='last'><a href='#'>Inventario</a></li>
 			</ul>
 		</li>
