e69f7bb068cea0d4e0647cbd9a91a2b65f9e690e
David Villalobos <davillafon12@gmail.com>
2015-02-01 22:52:22 -0600

    Cambio de scripts
    Ahora solo se usa el FINAL_LIMPIO

--- BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
+++ BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
@@ -1162,6 +1162,41 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+ALTER TABLE `tb_02_sucursal` CHANGE `Sucursal_Nombre` `Sucursal_Nombre` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+--
+-- Base de datos: `garotas_bonitas_main_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_39_configuracion`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_39_configuracion` (
+  `Parametro` varchar(50) NOT NULL,
+  `Valor` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`Parametro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tb_39_configuracion`
+--
+
+INSERT INTO `tb_39_configuracion` (`Parametro`, `Valor`) VALUES
+('cantidad_decimales', '2'),
+('codigo_empresa_traspaso_compras', '0'),
+('correo_administracion', 'esteban@garotasbonitascr.com'),
+('dolar_compra', '530.00'),
+('dolar_venta', '542.00'),
+('iva', '13'),
+('monto_intermedio_compra_cliente', '5000.00'),
+('monto_minimo_compra_cliente', '2500.00'),
+('porcentaje_retencion_tarjetas_hacienda', '2'),
+('tiempo_sesion', '600'),
+('ultima_actualizacion_estado_clientes', NULL);
   
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
