99e286e07412a163ff317b73734d08cda4afcad7
David Villalobos <davillafon12@gmail.com>
2015-03-22 00:13:25 -0600

    Cambiar footer

--- application/views/Footer/Default_Footer.php
+++ application/views/Footer/Default_Footer.php
@@ -4,26 +4,12 @@
 		<table class='tablaAcerca'>
 			<tr>
 				<td>
-				   <a class='acerca' href='#'>Acerca de . . .</a>
-				</td>
-				<td>
-				   <p class='copyright'>Web System Developed by Siviany Prendas & David Villalobos</p>
+					<p class='copyright'>Garotas Bonitas S.A.</p>				   
 				</td>
 			</tr>	
 			<tr>
 				<td>
-				   <a class='acerca' href='#'>Contacte al administrador del sitio</a>
-				</td>
-				<td>
 				   <p class='copyright'>Copyright &copy; 2014</p>
-				</td>
-			</tr>
-			<tr>
-				<td>
-				   <a class='acerca' href='#'>&iquest;Necesita ayuda?</a>
-				</td>
-				<td>
-				   <p class='copyright'>Garotas Bonitas S.A.</p>
 				</td>
 			</tr>
 		</table>";
