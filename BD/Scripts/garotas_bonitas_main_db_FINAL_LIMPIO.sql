a6a5ef7a6071036c08b2cf94e3b5cf5d6398ffb1
David Villalobos <davillafon12@gmail.com>
2015-03-23 00:04:11 -0600

    Actualizar Scripts BD

--- BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
+++ BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
@@ -1271,6 +1271,53 @@ ALTER TABLE `tb_43_deposito_recibo` CHANGE `Id` `Id` INT(11) NOT NULL AUTO_INCRE
 
 ALTER TABLE tb_27_notas_credito DROP INDEX Consecutivo_UNIQUE;
   
+CREATE TABLE IF NOT EXISTS `GAROTAS_BONITAS_MAIN_DB`.`TB_44_Traspaso_Inventario` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Sucursal_Salida` INT NOT NULL,
+  `Sucursal_Entrada` INT NOT NULL,
+  `Fecha` TIMESTAMP NULL,
+  `Usuario` INT NOT NULL,
+  `Factura_Traspasada` INT NOT NULL,
+  PRIMARY KEY (`Id`, `Sucursal_Salida`, `Sucursal_Entrada`, `Usuario`, `Factura_Traspasada`),
+  INDEX `fk_TB_44_Traspaso_Inventario_TB_02_Sucursal1_idx` (`Sucursal_Salida` ASC),
+  INDEX `fk_TB_44_Traspaso_Inventario_TB_02_Sucursal2_idx` (`Sucursal_Entrada` ASC),
+  INDEX `fk_TB_44_Traspaso_Inventario_TB_01_Usuario1_idx` (`Usuario` ASC),
+  INDEX `fk_TB_44_Traspaso_Inventario_TB_07_Factura1_idx` (`Factura_Traspasada` ASC),
+  CONSTRAINT `fk_TB_44_Traspaso_Inventario_TB_02_Sucursal1`
+    FOREIGN KEY (`Sucursal_Salida`)
+    REFERENCES `GAROTAS_BONITAS_MAIN_DB`.`TB_02_Sucursal` (`Codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_TB_44_Traspaso_Inventario_TB_02_Sucursal2`
+    FOREIGN KEY (`Sucursal_Entrada`)
+    REFERENCES `GAROTAS_BONITAS_MAIN_DB`.`TB_02_Sucursal` (`Codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_TB_44_Traspaso_Inventario_TB_01_Usuario1`
+    FOREIGN KEY (`Usuario`)
+    REFERENCES `GAROTAS_BONITAS_MAIN_DB`.`TB_01_Usuario` (`Usuario_Codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_TB_44_Traspaso_Inventario_TB_07_Factura1`
+    FOREIGN KEY (`Factura_Traspasada`)
+    REFERENCES `GAROTAS_BONITAS_MAIN_DB`.`TB_07_Factura` (`Factura_Consecutivo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE IF NOT EXISTS `GAROTAS_BONITAS_MAIN_DB`.`TB_45_Articulos_Traspaso_Inventario` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Codigo` VARCHAR(20) NULL,
+  `Descripcion` VARCHAR(150) NULL,
+  `Cantidad` INT NULL,
+  `Traspaso` INT NOT NULL,
+  PRIMARY KEY (`Id`, `Traspaso`),
+  INDEX `fk_TB_45_Articulos_Traspaso_Inventario_TB_44_Traspaso_Inven_idx` (`Traspaso` ASC),
+  CONSTRAINT `fk_TB_45_Articulos_Traspaso_Inventario_TB_44_Traspaso_Inventa1`
+    FOREIGN KEY (`Traspaso`)
+    REFERENCES `GAROTAS_BONITAS_MAIN_DB`.`TB_44_Traspaso_Inventario` (`Id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);  
+ 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
