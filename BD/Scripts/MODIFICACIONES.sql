772a9b315bc52b3135bab053efc3e0a652959119
David Villalobos <davillafon12@gmail.com>
2015-05-02 17:07:21 -0600

    Cambios en los scripts de la BD
    Aplicar retencion por cliente

--- BD/Scripts/MODIFICACIONES.sql
+++ BD/Scripts/MODIFICACIONES.sql
@@ -1,12 +1,5 @@
-UPDATE `garotas_bonitas_main_db`.`tb_03_cliente` SET `Cliente_Numero_Pago` = '1' WHERE `tb_03_cliente`.`Cliente_Cedula` = 1;
-UPDATE `garotas_bonitas_main_db`.`tb_03_cliente` SET `Cliente_Numero_Pago` = '2' WHERE `tb_03_cliente`.`Cliente_Cedula` = 0;
-
--- Creación de Usuario de Consultas con permisos de lectura y ejecución de Procedimientos 
-
-create user 'consulta'@'%' identified by 'consulta';
-GRANT SELECT ON garotas_bonitas_main_db . * TO  'consulta'@'%'
-IDENTIFIED BY  'consulta'
-GRANT SELECT , EXECUTE ON `garotas\_bonitas\_main\_db` . * TO 'consulta'@'%';
+ALTER TABLE `tb_03_cliente` ADD `Aplica_Retencion` BOOLEAN NOT NULL AFTER `Cliente_EsExento`;
+UPDATE `tb_03_cliente` SET `Aplica_Retencion` = 0
 
 
 
--- BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
+++ BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
@@ -1326,4 +1326,14 @@ ALTER TABLE `tb_08_articulos_factura` ADD `Articulo_Factura_Precio_Final` DOUBLE
 
 ALTER TABLE `tb_04_articulos_proforma` ADD `Articulo_Proforma_Precio_Final` DOUBLE NOT NULL AFTER `Articulo_Proforma_Precio_Unitario`;
 
-ALTER TABLE `tb_10_proforma` ADD `Proforma_Retencion` DOUBLE NOT NULL AFTER `Proforma_Porcentaje_IVA`; 
+ALTER TABLE `tb_10_proforma` ADD `Proforma_Retencion` DOUBLE NOT NULL AFTER `Proforma_Porcentaje_IVA`;
+
+UPDATE `garotas_bonitas_main_db`.`tb_03_cliente` SET `Cliente_Numero_Pago` = '1' WHERE `tb_03_cliente`.`Cliente_Cedula` = 1;
+UPDATE `garotas_bonitas_main_db`.`tb_03_cliente` SET `Cliente_Numero_Pago` = '2' WHERE `tb_03_cliente`.`Cliente_Cedula` = 0;
+
+-- Creación de Usuario de Consultas con permisos de lectura y ejecución de Procedimientos 
+
+create user 'consulta'@'%' identified by 'consulta';
+GRANT SELECT ON garotas_bonitas_main_db . * TO  'consulta'@'%'
+IDENTIFIED BY  'consulta'
+GRANT SELECT , EXECUTE ON `garotas\_bonitas\_main\_db` . * TO 'consulta'@'%'; 
