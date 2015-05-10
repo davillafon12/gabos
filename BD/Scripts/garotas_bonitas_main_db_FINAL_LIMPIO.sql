9535d0ee1ead60ed55b80a6c659d4030c73076be
David Villalobos <davillafon12@gmail.com>
2015-05-10 00:45:42 -0600

    Correciones de la BD
    Correcciones según el commit de correcciones de exento y retención

--- BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
+++ BD/Scripts/garotas_bonitas_main_db_FINAL_LIMPIO.sql
@@ -1336,4 +1336,14 @@ UPDATE `garotas_bonitas_main_db`.`tb_03_cliente` SET `Cliente_Numero_Pago` = '2'
 create user 'consulta'@'%' identified by 'consulta';
 GRANT SELECT ON garotas_bonitas_main_db . * TO  'consulta'@'%'
 IDENTIFIED BY  'consulta'
-GRANT SELECT , EXECUTE ON `garotas\_bonitas\_main\_db` . * TO 'consulta'@'%'; 
+GRANT SELECT , EXECUTE ON `garotas\_bonitas\_main\_db` . * TO 'consulta'@'%';
+
+
+ALTER TABLE `tb_03_cliente` ADD `Aplica_Retencion` BOOLEAN NOT NULL AFTER `Cliente_EsExento`;
+UPDATE `tb_03_cliente` SET `Aplica_Retencion` = 0;
+ALTER TABLE `tb_06_articulo` ADD `Articulo_No_Retencion` BOOLEAN NOT NULL AFTER `Articulo_Exento`;
+UPDATE `tb_06_articulo` SET `Articulo_No_Retencion` = 0;
+ALTER TABLE `tb_08_articulos_factura` ADD `Articulo_Factura_No_Retencion` BOOLEAN NOT NULL AFTER `Articulo_Factura_Exento`;
+update `tb_08_articulos_factura` set Articulo_Factura_No_Retencion = 0;
+ALTER TABLE `tb_04_articulos_proforma` ADD `Articulo_Proforma_No_Retencion` BOOLEAN NOT NULL AFTER `Articulo_Proforma_Exento`;
+update `tb_04_articulos_proforma` set Articulo_Proforma_No_Retencion = 0; 
