59ade0a58cf4c1ff4ad783a3ac00918ce041bb8f
David Villalobos <davillafon12@gmail.com>
2015-11-08 21:11:06 -0600

    BD | Modificaciones de la BD
    Modificaciones según los cambios realizados al 8 de noviembre

--- BD/Scripts/MODIFICACIONES.sql
+++ BD/Scripts/MODIFICACIONES.sql
@@ -1,8 +1,20 @@
+CREATE TABLE IF NOT EXISTS `TB_47_Descuento_Proforma` (
+  `Id` INT NOT NULL,
+  `Proforma` INT NOT NULL,
+  `Sucursal` INT NOT NULL,
+  PRIMARY KEY (`Id`),
+  INDEX `fk_TB_47_Descuento_Proforma_TB_10_Proforma1_idx` (`Proforma` ASC),
+  INDEX `fk_TB_47_Descuento_Proforma_TB_02_Sucursal1_idx` (`Sucursal` ASC),
+  CONSTRAINT `fk_TB_47_Descuento_Proforma_TB_10_Proforma1`
+    FOREIGN KEY (`Proforma`)
+    REFERENCES `TB_10_Proforma` (`Proforma_Consecutivo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_TB_47_Descuento_Proforma_TB_02_Sucursal1`
+    FOREIGN KEY (`Sucursal`)
+    REFERENCES `TB_02_Sucursal` (`Codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-
-CREATE TABLE IF NOT EXISTS `tb_46_relacion_desampa` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Consecutivo` int(11) NOT NULL,
-  `Documento` varchar(50) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+ALTER TABLE `tb_26_recibos_dinero` ADD `Comentarios` VARCHAR(1000) NOT NULL AFTER `Tipo_Pago`;
