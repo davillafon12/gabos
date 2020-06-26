aac12c32c36755abde78bb82c9a62ffd1a0d0814
root <root@debian>
2020-06-26 06:28:38 -0600

    Configuracion Prod

--- updateEstados.sh
+++ updateEstados.sh
@@ -2,7 +2,7 @@
 
 RUTA_LOG="/var/log/gabos/updater/$(date +'%Y_%m_%d').log"
 HOST_GABO=127.0.0.1
-PUERTO_GABO=8080
+PUERTO_GABO=8181
 
 URL_REENVIO="$HOST_GABO:$PUERTO_GABO/external/enviarComprobantesAHacienda"
 URL_ACTUALIZAR="$HOST_GABO:$PUERTO_GABO/external/actualizarComprobantes"
@@ -11,7 +11,7 @@ URL_ACTUALIZAR="$HOST_GABO:$PUERTO_GABO/external/actualizarComprobantes"
 echo "$(date +'%H:%M') | Enviando comprobantes!!!" >> $RUTA_LOG
 echo "$(date +'%H:%M') | Executing: wget curl $URL_REENVIO >> $RUTA_LOG" >> $RUTA_LOG
 
-curl "${URL_REENVIO}" >> "${RUTA_LOG}"
+wget "${URL_REENVIO}" -O ->> "${RUTA_LOG}"
 
 echo "$(date +'%H:%M') | Enviar comprobantes | DONE" >> $RUTA_LOG
 
@@ -20,7 +20,7 @@ echo "$(date +'%H:%M') | Enviar comprobantes | DONE" >> $RUTA_LOG
 echo "$(date +'%H:%M') | Actualizando estados!!!" >> $RUTA_LOG
 echo "$(date +'%H:%M') | Executing: curl $URL_ACTUALIZAR >> $RUTA_LOG" >> $RUTA_LOG
 
-curl "${URL_ACTUALIZAR}" >> "${RUTA_LOG}"
+wget "${URL_ACTUALIZAR}" -O ->> "${RUTA_LOG}"
 
 echo "$(date +'%H:%M') | Actualizar estados | DONE" >> $RUTA_LOG
 
