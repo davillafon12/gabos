12928c13eb0c5bdcce852db2a74ea0951a0ea0a0
David Villalobos <davillafon12@gmail.com>
2020-09-05 14:01:30 -0600

    Merge Fixes

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 GABOS
 =====
 
-Sistema de Facturación para la empresa <i>Inversiones Garotas Bonitas SA</i><br>
+Version del sistema GABO con HTTPS habilitado en JS <br>
+Sistema de Facturacion <br>
 
-Esta versión ya contempla la factura electrónica según las resoluciones del Ministerio de Hacienda del año 2016 sobre comprobantes electrónicos.<br>
-
-El repositorio se ha limpiado puesto que tenia información innecesaria.<br>
-
-NOTA: FALTA MIGRAR SERVIDOR DE IMPRESIÓN AL MISMO GABO, PARA ELIMINAR EL TOMCAT.<br>
-
-Desarrollador:<br>
 -David Villalobos Fonseca<br>
-
-Desarrollo de Reportes:<br>
--Siviany Prendas Zamora (Solo reportería)
+-Siviany Prendas Zamora
--- Reportes/USUARIO/PROCEDIMIENTOS/Procedure - ListaDefacturasPorUsuario.sql
+++ Reportes/USUARIO/PROCEDIMIENTOS/Procedure - ListaDefacturasPorUsuario.sql
@@ -1,9 +1,7 @@
-DROP PROCEDURE `gabo_fe`.`PA_ListaDeFacturasPorUsuario`;
-
-
-USE gabo_fe; 
+USE gabo_trueque; 
 DELIMITER ;;
-CREATE DEFINER = 'consulta'@'%' PROCEDURE PA_ListaDeFacturasPorUsuario(
+CREATE DEFINER = 'consulta'@'%' PROCEDURE PA_ListaDeFacturasPorUsuario
+(
 	IN paEstadoFactura VARCHAR(30),
 	IN paFechaI VARCHAR(30), 
 	IN paFechaF VARCHAR(30),
@@ -12,9 +10,9 @@ CREATE DEFINER = 'consulta'@'%' PROCEDURE PA_ListaDeFacturasPorUsuario(
 	IN paSuDesamparados VARCHAR(10),
 	IN paSuGarotasBonitas VARCHAR(10)
  )
-BEGIN
+ BEGIN
 	SET @wherePrincipal 		= CONCAT(' where   user.TB_02_Sucursal_Codigo = ', '\'', paSucursal, '\'', 
-								 ' and fac.Factura_Estado in (',paEstadoFactura,') ',
+								 ' and fac.Factura_Estado in (', paEstadoFactura, ') ',
 								 ' and UNIX_TIMESTAMP(fac.Factura_Fecha_Hora) BETWEEN UNIX_TIMESTAMP(', '\'', paFechaI, '\'', 
 								 ') AND UNIX_TIMESTAMP(', '\'', paFechaF, '\'', ')'); 
 	SET @whereCliente = CONCAT(' and cli.Cliente_EsSucursal = ', '\'', paEsSucursal, '\'');								 
@@ -24,22 +22,18 @@ BEGIN
 											fac.Factura_Fecha_Hora fecha, 
 											fac.Factura_Monto_Total montoTotal, 
 											fac.Factura_Monto_IVA montoIVA, 
-											fac.Factura_Monto_Sin_IVA montoSinIVA, 
-											suc.Sucursal_Nombre
+											fac.Factura_Monto_Sin_IVA montoSinIVA
 									FROM    tb_01_usuario user inner join 
-											tb_07_factura fac on user.Usuario_Codigo = fac.Factura_Vendedor_Codigo ');
-	SET @AgregarCliente = (' inner join tb_03_cliente cli on fac.TB_03_Cliente_Cliente_Cedula = cli.Cliente_Cedula  ');
-	SET @AgregarSucursal = (' inner join tb_02_sucursal suc on fac.tb_02_sucursal_codigo = suc.Codigo ');	 
-	SET @wherePrinGarotas =	CONCAT(' and  user.TB_02_Sucursal_Codigo = fac.TB_02_Sucursal_Codigo ', @AgregarCliente);	
-	SET @wherePrinDesampa =	CONCAT(' and  user.TB_02_Sucursal_Codigo = fac.Factura_Vendedor_Sucursal ', @AgregarCliente);			
+											tb_07_factura fac on user.Usuario_Codigo = fac.Factura_Vendedor_Codigo and ');
+	SET @wherePrinGarotas =	(' user.TB_02_Sucursal_Codigo = fac.TB_02_Sucursal_Codigo inner join 
+								tb_03_cliente cli on fac.TB_03_Cliente_Cliente_Cedula = cli.Cliente_Cedula ');	
+	SET @wherePrinDesampa =	(' user.TB_02_Sucursal_Codigo = fac.Factura_Vendedor_Sucursal inner join 
+								tb_03_cliente cli on fac.TB_03_Cliente_Cliente_Cedula = cli.Cliente_Cedula ');												
 	IF paSuDesamparados = 'true' AND paSuGarotasBonitas = 'false' then 
-		SET @QUERY = CONCAT(@QUERY,@wherePrinDesampa, @AgregarSucursal);
+		SET @QUERY = CONCAT(@QUERY,@wherePrinDesampa);
 	END IF;
 	IF paSuDesamparados = 'false' AND paSuGarotasBonitas = 'true' then 
-		SET @QUERY = CONCAT(@QUERY, @wherePrinGarotas, @AgregarSucursal);
-	END IF;
-	IF paSuDesamparados = 'false' AND paSuGarotasBonitas = 'false' then 
-		SET @QUERY = CONCAT(@QUERY, @wherePrinGarotas, @AgregarSucursal);
+		SET @QUERY = CONCAT(@QUERY, @wherePrinGarotas);
 	END IF;
 	SET @QUERYDESAMPA =  CONCAT( ' inner join tb_46_relacion_trueque des on fac.Factura_Consecutivo  = des.Consecutivo and ' , 
 									' des.Documento = \'factura\'');
@@ -55,11 +49,8 @@ BEGIN
 	IF paSuDesamparados = 'false' AND paSuGarotasBonitas = 'true' then 
 		SET @QUERY = CONCAT(@QUERY, @wherePrincipal, @whereCliente, @WHEREDESAMPA, ' order by user.Usuario_Codigo, fac.Factura_Fecha_Hora');
 	END IF;
-	IF paSuDesamparados = 'false' AND paSuGarotasBonitas = 'false' then 
-		SET @QUERY = CONCAT(@QUERY, @wherePrincipal, @whereCliente, ' order by user.Usuario_Codigo, fac.Factura_Fecha_Hora');
-	END IF;
 	IF paSuDesamparados = 'true' AND paSuGarotasBonitas = 'true' then 
-		SET @QUERY = CONCAT(@QUERY,@AgregarSucursal,@AgregarCliente, @wherePrincipal, @whereCliente, ' order by user.Usuario_Codigo, fac.Factura_Fecha_Hora');
+		SET @QUERY = CONCAT(@QUERY, @wherePrincipal, @whereCliente, ' order by user.Usuario_Codigo, fac.Factura_Fecha_Hora');
 	END IF;
   -- select @QUERY as 'Resultado';  
   -- preparamos el objete Statement a partir de nuestra variable
