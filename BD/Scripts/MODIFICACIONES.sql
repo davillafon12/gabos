a6ac0baa22e1a95a124aa6fd046c06a9b56212fb
David Villalobos <davillafon12@gmail.com>
2022-04-19 18:36:11 -0600

    DB Statements

--- BD/Scripts/MODIFICACIONES.sql
+++ BD/Scripts/MODIFICACIONES.sql
@@ -1,7 +1,1 @@
-ALTER TABLE `tb_56_articulos_factura_electronica` ADD `CodigoCabys` VARCHAR( 14 ) NOT NULL AFTER `TipoCodigo`;
-
-ALTER TABLE `tb_58_articulos_nota_credito_electronica` ADD `CodigoCabys` VARCHAR( 14 ) NOT NULL AFTER `TipoCodigo`;
-
-ALTER TABLE `tb_62_articulos_factura_compra_electronica` ADD `CodigoCabys` VARCHAR( 14 ) NOT NULL AFTER `TipoCodigo`;
-
-ALTER TABLE `tb_28_productos_notas_credito` ADD `CodigoCabys` VARCHAR( 14 ) NOT NULL AFTER `TipoCodigo`;
+UPDATE tb_06_articulo SET Articulo_Cantidad_Inventario = 0, Articulo_Cantidad_Defectuoso = 0 WHERE TB_02_Sucursal_Codigo = 2;
