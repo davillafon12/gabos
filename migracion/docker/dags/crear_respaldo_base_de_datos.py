4ac4dba748b1f8c14f176dc8c2b160db0c2da46c
David Villalobos <davillafon12@gmail.com>
2023-07-13 18:29:12 -0600

    eliminar respaldos viejos y mover a carpeta sync

--- migracion/docker/dags/crear_respaldo_base_de_datos.py
+++ migracion/docker/dags/crear_respaldo_base_de_datos.py
@@ -7,6 +7,8 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 
 backup_name = "{{ ds }}_database_backup_file"
+backup_directory = Variable.get("ruta_respaldos_bd")
+backup_expiration_in_days = Variable.get("dias_para_eliminar_respaldo")
 
 dag = DAG(
     "Respaldar_Base_De_Datos",
@@ -26,16 +28,30 @@ crear_respaldo = SSHOperator(ssh_conn_id='gabo_ssh',
 
 comprimir_respaldo = SSHOperator(ssh_conn_id='gabo_ssh',
     task_id='comprimir_respaldo',
-    conn_timeout=300,
-    cmd_timeout=300,
-    command='tar -czvf ~/respaldos/' + backup_name + '.tar.gz /tmp/' + backup_name + '.sql',
+    conn_timeout=1200,
+    cmd_timeout=1200,
+    command='tar -czvf /tmp/' + backup_name + '.tar.gz /tmp/' + backup_name + '.sql',
     dag=dag)
 
-eliminar_respaldo = SSHOperator(ssh_conn_id='gabo_ssh',
-    task_id='eliminar_respaldo',
+mover_respaldo = SSHOperator(ssh_conn_id='gabo_ssh',
+    task_id='mover_respaldo',
+    conn_timeout=60,
+    cmd_timeout=60,
+    command='mv /tmp/' + backup_name + '.tar.gz ' + backup_directory,
+    dag=dag)
+
+eliminar_respaldo_sin_comprimir = SSHOperator(ssh_conn_id='gabo_ssh',
+    task_id='eliminar_respaldo_sin_comprimir',
     conn_timeout=60,
     cmd_timeout=60,
     command='rm /tmp/' + backup_name + '.sql',
     dag=dag)
+
+eliminar_respaldos_viejos = SSHOperator(ssh_conn_id='gabo_ssh',
+    task_id='eliminar_respaldos_viejos',
+    conn_timeout=60,
+    cmd_timeout=60,
+    command="find " + backup_directory + " -type f -mtime +" + backup_expiration_in_days + " -name '*.gz' -print0 | xargs -r0 rm --",
+    dag=dag)
     
-crear_respaldo >> comprimir_respaldo >> eliminar_respaldo
+crear_respaldo >> comprimir_respaldo >> [eliminar_respaldo_sin_comprimir, mover_respaldo] >> eliminar_respaldos_viejos
