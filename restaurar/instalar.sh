22ccca614611ceb136ecd2816ed009d02739e39f
David Villalobos <davillafon12@gmail.com>
2024-09-23 19:12:24 -0600

    primera version instalar.sh

--- restaurar/instalar.sh
+++ restaurar/instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update
-apt -y install rsync openssh-server sshpass ca-certificates curl git
+sudo apt update
+sudo apt -y install rsync openssh-server sshpass ca-certificates curl git
 
 primeraVez="si"
 
@@ -9,18 +9,18 @@ primeraVez="si"
 if [ ! -x "$(command -v docker)" ]; then
     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+    # Add the repository to Apt sources:
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
 
-    apt-get update
-
-    apt-get y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 
